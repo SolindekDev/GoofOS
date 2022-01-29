d35986f994dafe760c210f9ed9a8d0c86ddd3a60
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2022-01-29 19:21:45 +0100

    Gitignore

---
+++ build.rb
@@ -0,0 +1,23 @@
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/kernel.c -o kernel/kernel.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/ports.c -o kernel/ports.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/screen.c -o kernel/screen.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/string.c -o kernel/string.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/util.c -o kernel/util.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/keyboard.c -o kernel/keyboard.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/disk.c -o kernel/disk.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/mem.c -o kernel/mem.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/power.c -o kernel/power.o"
+
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/cpu/idt.c -o kernel/cpu/idt.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/cpu/isr.c -o kernel/cpu/isr.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/cpu/timer.c -o kernel/cpu/timer.o"
+system "x86_64-elf-gcc -g -m32 -ffreestanding -c kernel/cpu/cpu.c -o kernel/cpu/cpu.o"
+
+system "nasm kernel/cpu/interrupt.asm -felf -o kernel/cpu/interrupt.o"
+system "nasm kernel/shutdown.asm -felf -o kernel/shutdown.o"
+
+system "x86_64-elf-ld -m elf_i386 -o kernel.bin -Ttext 0x1000 boot/kernel_entry.o kernel/shutdown.o kernel/mem.o kernel/power.o kernel/kernel.o kernel/cpu/cpu.o kernel/cpu/interrupt.o kernel/disk.o kernel/cpu/idt.o kernel/cpu/isr.o kernel/cpu/timer.o kernel/ports.o kernel/screen.o kernel/keyboard.o kernel/util.o kernel/string.o --oformat binary"
+
+system "cat boot/mbr.bin kernel.bin > os-image.bin"
+
+system "qemu-system-x86_64 -drive format=raw,file=os-image.bin,index=0,if=floppy, -m 1G"
--- builder/build.rb
+++ builder/build.rb
@@ -16,137 +16,137 @@
 # 
 ##########################################################################
 
-# require 'rbconfig'
+require 'rbconfig'
 
-# $os = "Other"
+$os = "Other"
 
-# def os()
-#   @os ||= (
-#     host_os = RbConfig::CONFIG['host_os']
-#     case host_os
-#     when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-#         $os = "Win"
-#     when /darwin|mac os/
-#         $os = "MacOS"
-#     when /linux/
-#         $os = "Linux"
-#     when /solaris|bsd/
-#         $os = "Unix"
-#     else
-#         $os = "Other"
-#     end
-#   )
-# end
+def os()
+  @os ||= (
+    host_os = RbConfig::CONFIG['host_os']
+    case host_os
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+        $os = "Win"
+    when /darwin|mac os/
+        $os = "MacOS"
+    when /linux/
+        $os = "Linux"
+    when /solaris|bsd/
+        $os = "Unix"
+    else
+        $os = "Other"
+    end
+  )
+end
 
-# os()
+os()
 
-# class String
-#     def black;          "\033[30m#{self}\033[0m" end
-#     def red;            "\033[31m#{self}\033[0m" end
-#     def green;          "\033[32m#{self}\033[0m" end
-#     def brown;          "\033[33m#{self}\033[0m" end
-#     def blue;           "\033[34m#{self}\033[0m" end
-#     def magenta;        "\033[35m#{self}\033[0m" end
-#     def cyan;           "\033[36m#{self}\033[0m" end
-#     def gray;           "\033[37m#{self}\033[0m" end
-# end
+class String
+    def black;          "\033[30m#{self}\033[0m" end
+    def red;            "\033[31m#{self}\033[0m" end
+    def green;          "\033[32m#{self}\033[0m" end
+    def brown;          "\033[33m#{self}\033[0m" end
+    def blue;           "\033[34m#{self}\033[0m" end
+    def magenta;        "\033[35m#{self}\033[0m" end
+    def cyan;           "\033[36m#{self}\033[0m" end
+    def gray;           "\033[37m#{self}\033[0m" end
+end
 
-# asm = "nasm"
-# gcc = "x86_64-elf-gcc"
-# ld = "x86_64-elf-ld"
-# cat = "cat"
-# qemu = "qemu-system-x86_64"
+asm = "nasm"
+gcc = "x86_64-elf-gcc"
+ld = "x86_64-elf-ld"
+cat = "cat"
+qemu = "qemu-system-x86_64"
 
-# gcc_flags = "-ffreestanding -m32 -g -c"
-# ld_flags_start = "-m elf_i386 -o kernel.bin -Ttext 0x1000"
-# ld_flags_end = "--oformat binary"
-# asm_flags_elf = "-f elf"
-# asm_flags_bin = "-f bin"
-# qemu_flags = "-drive format=raw,file=os.bin -d cpu_reset -monitor stdio -m 1G"
+gcc_flags = "-ffreestanding -m32 -g -c"
+ld_flags_start = "-m elf_i386 -o kernel.bin -Ttext 0x1000"
+ld_flags_end = "--oformat binary"
+asm_flags_elf = "-f elf"
+asm_flags_bin = "-f bin"
+qemu_flags = "-drive format=raw,file=os.bin -d cpu_reset -monitor stdio -m 1G"
 
-# bootloader_dir = './src/bootloader/'
-# kernel_dir = './src/kernel/'
+bootloader_dir = './src/bootloader/'
+kernel_dir = './src/kernel/'
 
-# builder_goof = "Builder Goof"
-# builder_goof_builder_boot = "Builder Goof Bootloader"
-# builder_goof_builder_kernel = "Builder Goof Kernel"
-# builder_goof_builder_ld = "Builder Goof Linked"
+builder_goof = "Builder Goof"
+builder_goof_builder_boot = "Builder Goof Bootloader"
+builder_goof_builder_kernel = "Builder Goof Kernel"
+builder_goof_builder_ld = "Builder Goof Linked"
 
-# files_asm = Dir.glob(File.join(bootloader_dir, '**', '*.asm')).select{|file| File.file?(file)}
-# files_kernel_c = Dir.glob(File.join(kernel_dir, '**', '*.c')).select{|file| File.file?(file)}
-# files_kernel_asm = Dir.glob(File.join(kernel_dir, '**', '*.asm')).select{|file| File.file?(file)}
+files_asm = Dir.glob(File.join(bootloader_dir, '**', '*.asm')).select{|file| File.file?(file)}
+files_kernel_c = Dir.glob(File.join(kernel_dir, '**', '*.c')).select{|file| File.file?(file)}
+files_kernel_asm = Dir.glob(File.join(kernel_dir, '**', '*.asm')).select{|file| File.file?(file)}
 
-# puts " "
-# puts " #{"Goof OS Builder - Written in Ruby".magenta}"
-# puts " "
+puts " "
+puts " #{"Goof OS Builder - Written in Ruby".magenta}"
+puts " "
 
-# puts "----------------------------------------------------------"
-# puts "[#{builder_goof.magenta}] Compiling bootloader (.asm)..."
+puts "----------------------------------------------------------"
+puts "[#{builder_goof.magenta}] Compiling bootloader (.asm)..."
 
-# files_asm.each do |f|
-#     if f == "./src/bootloader/entering/kernel_entry.asm"
-#         system "#{asm} ./src/bootloader/entering/kernel_entry.asm #{asm_flags_elf} -o ./src/bootloader/entering/kernel_entry.o"
-#         puts "[#{builder_goof_builder_boot.magenta}] Compiled #{f}"
-#     else
-#         system "#{asm} #{f} #{asm_flags_bin} -o #{f.gsub(".asm", ".o")}"
-#         puts "[#{builder_goof_builder_boot.magenta}] Compiled #{f}"
-#     end
-# end
+files_asm.each do |f|
+    if f == "./src/bootloader/entering/kernel_entry.asm"
+        system "#{asm} ./src/bootloader/entering/kernel_entry.asm #{asm_flags_elf} -o ./src/bootloader/entering/kernel_entry.o"
+        puts "[#{builder_goof_builder_boot.magenta}] Compiled #{f}"
+    else
+        system "#{asm} #{f} #{asm_flags_bin} -o #{f.gsub(".asm", ".o")}"
+        puts "[#{builder_goof_builder_boot.magenta}] Compiled #{f}"
+    end
+end
 
-# puts "----------------------------------------------------------"
+puts "----------------------------------------------------------"
 
-# puts "[#{builder_goof.magenta}] Compiling kernel c files (.c)..."
+puts "[#{builder_goof.magenta}] Compiling kernel c files (.c)..."
 
-# files_kernel_c.each do |f|
-#     system "#{gcc} #{gcc_flags} #{f} -o #{f.gsub(".c", ".o")}"
-#     puts "[#{builder_goof_builder_kernel.magenta}] Compiled #{f}"
-# end
-# puts "----------------------------------------------------------"
-# puts "[#{builder_goof.magenta}] Compiling kernel assembler files (.asm)..."
+files_kernel_c.each do |f|
+    system "#{gcc} #{gcc_flags} #{f} -o #{f.gsub(".c", ".o")}"
+    puts "[#{builder_goof_builder_kernel.magenta}] Compiled #{f}"
+end
+puts "----------------------------------------------------------"
+puts "[#{builder_goof.magenta}] Compiling kernel assembler files (.asm)..."
 
-# files_kernel_asm.each do |f|
-#     system "#{asm} #{f} #{asm_flags_elf} -o #{f.gsub(".asm", ".o")}"
-#     puts "[#{builder_goof_builder_kernel.magenta}] Compiled #{f}"
-# end
-# puts "----------------------------------------------------------"
-# puts "[#{builder_goof.magenta}] Linking o files (.o)..."
+files_kernel_asm.each do |f|
+    system "#{asm} #{f} #{asm_flags_elf} -o #{f.gsub(".asm", ".o")}"
+    puts "[#{builder_goof_builder_kernel.magenta}] Compiled #{f}"
+end
+puts "----------------------------------------------------------"
+puts "[#{builder_goof.magenta}] Linking o files (.o)..."
 
-# files = ""
+files = ""
 
-# files_kernel_c.each do |f|
-#     files += f.gsub(".c", ".o") + " "
-# end
-# files_kernel_asm.each do |f|
-#     files += f.gsub(".asm", ".o") + " "
-# end
+files_kernel_c.each do |f|
+    files += f.gsub(".c", ".o") + " "
+end
+files_kernel_asm.each do |f|
+    files += f.gsub(".asm", ".o") + " "
+end
 
-# system "#{ld} #{ld_flags_start} ./src/bootloader/entering/kernel_entry.o #{files} #{ld_flags_end}"
-# system "#{cat} ./src/bootloader/bootloader.o kernel.bin ./src/bootloader/entering/times.o > os.bin"
-# puts "[#{builder_goof_builder_ld.magenta}] Linked kernel. Output in os.bin!"
-# puts "----------------------------------------------------------"
+system "#{ld} #{ld_flags_start} ./src/bootloader/entering/kernel_entry.o #{files} #{ld_flags_end}"
+system "#{cat} ./src/bootloader/bootloader.o kernel.bin ./src/bootloader/entering/times.o > os.bin"
+puts "[#{builder_goof_builder_ld.magenta}] Linked kernel. Output in os.bin!"
+puts "----------------------------------------------------------"
 
-# def emulator_is()
-#     print "[#{"Builder Goof".magenta}] Do you want to emulate the OS? (Y/N): "
-#     y = gets().chomp
-#     if y.downcase() == "y" || y.downcase() == "yes"
-#         system "qemu-system-x86_64 -drive format=raw,file=os.bin -d cpu_reset -monitor stdio -m 1G"
-#         if $os == "Win"
-#             system "cls"
-#         else
-#             system "clear"
-#         end
-#         puts "----------------------------------------------------------"
-#         puts " "
-#         puts "                  #{"Stop emulate the OS...".magenta}"
-#         puts " "
-#         puts "----------------------------------------------------------"
-#     elsif y.downcase() == "n" || y.downcase() == "no"
-#         puts "[#{"Exit".red}] You don't want to emulate the OS so builder exit.\n"
-#         exit 1
-#     else
-#         puts "[#{"Error".red}] Unknown value. expected (Y/N)"
-#         emulator_is()
-#     end
-# end 
+def emulator_is()
+    print "[#{"Builder Goof".magenta}] Do you want to emulate the OS? (Y/N): "
+    y = gets().chomp
+    if y.downcase() == "y" || y.downcase() == "yes"
+        system "qemu-system-x86_64 -drive format=raw,file=os.bin -d cpu_reset -monitor stdio -m 1G"
+        if $os == "Win"
+            system "cls"
+        else
+            system "clear"
+        end
+        puts "----------------------------------------------------------"
+        puts " "
+        puts "                  #{"Stop emulate the OS...".magenta}"
+        puts " "
+        puts "----------------------------------------------------------"
+    elsif y.downcase() == "n" || y.downcase() == "no"
+        puts "[#{"Exit".red}] You don't want to emulate the OS so builder exit.\n"
+        exit 1
+    else
+        puts "[#{"Error".red}] Unknown value. expected (Y/N)"
+        emulator_is()
+    end
+end 
 
-# emulator_is()
+emulator_is()
